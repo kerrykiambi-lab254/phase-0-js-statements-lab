695cf9a5e7e9c3a1cab210457c09de15ace54712
kerrykiambi-lab254 <kerry.kiambi@student.moringaschool.com>
2026-03-27 18:23:09 +0000

    first

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,20 @@
-const temperature = 90;
-const timeOfDay = "morning";
-let soilMoisture = 30;
+// gardenManagement.js
+// Minimal script that runs on require and prints only the expected outputs.
+
+const temperature = 85;
+if (temperature > 80) {
+  console.log('Watering on');
+}
+
+const timeOfDay = 'morning';
+if (timeOfDay === 'morning') {
+  console.log('Lights off');
+}
+
+let soilMoisture = 35;
+while (soilMoisture < 40) {
+  soilMoisture++;
+}
+console.log(soilMoisture);
+
+module.exports = { temperature, timeOfDay, soilMoisture };
